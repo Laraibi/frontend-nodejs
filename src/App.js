a85b315ff5a82390210768ca24643f7c6930f825
Laraibi <meh.laraibi@gmail.com>
2023-01-16 22:40:47 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 function App() {
+  const [personnas, setPersonnas] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/", {
+        headers: { "Access-Control-Allow-Origin": "*" },
+      })
+      .then((res) => {
+        // console.log(res.data)
+        setPersonnas(res.data.personas);
+      });
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {personnas.map((p, key) => (
+          <li key={key}>{p.name}</li>
+        ))}
+      </ul>
+      <input type="text" value={}/>
     </div>
   );
 }
