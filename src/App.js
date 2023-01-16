49f2266341d9cf944a1efaa5d80b73a24dba137c
Laraibi <meh.laraibi@gmail.com>
2023-01-17 00:24:56 +0100

    crud

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 function App() {
   const [personnas, setPersonnas] = useState([]);
+  const [newPerson, setNewPerson] = useState({ name: "", age: "" });
   useEffect(() => {
     axios
       .get("http://localhost:3000/", {
@@ -13,7 +14,9 @@ function App() {
         // console.log(res.data)
         setPersonnas(res.data.personas);
       });
-  });
+  }, []);
+  // il y'a une dipendency
+
   return (
     <div className="App">
       <ul>
@@ -21,7 +24,29 @@ function App() {
           <li key={key}>{p.name}</li>
         ))}
       </ul>
-      <input type="text" value={}/>
+      {/* <input type="text" value={}/>
+       */}
+      <input
+        type="text"
+        value={newPerson.name}
+        onChange={(e) => setNewPerson({ ...newPerson, name: e.target.value })}
+      />
+      <input
+        type="text"
+        value={newPerson.age}
+        onChange={(e) => setNewPerson({ ...newPerson, age: e.target.value })}
+      />
+      <button
+        onClick={() =>
+          axios
+            .post("http://localhost:3000/person", newPerson, {
+              headers: { "Access-Control-Allow-Origin": "*" },
+            })
+            .then((r) => setPersonnas([r.data]))
+        }
+      >
+        Ajouter
+      </button>
     </div>
   );
 }
